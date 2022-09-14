1b5dd7b07654f54ceccdd77140a7e7efe8f29561
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-09-14 17:01:32 +0200

    Commit TestListadoJuegos

---
+++ Grupo01_LucaSteam_v01/test/data/TestListadoJuegos.java
@@ -0,0 +1,68 @@
+package data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.Juegos;
+
+import java.util.ArrayList;
+
+
+public class TestListadoJuegos {
+	
+	/**
+	 * Parte logger
+	 * 
+	 */
+
+	private static byte cont = 1;
+	private static Logger logger;
+
+	// Inicializo
+	static {
+		try {
+			logger = LogManager.getLogger(TestListadoJuegos.class);
+		} catch (Throwable e) {
+			System.out.println("Don't work");
+		}
+	}
+
+	@BeforeClass
+	public static void onceExecutedBeforeAll() {
+		logger.info(">>> Iniciando las pruebas");
+	}
+
+	@Before
+	public void executedBeforeEach() {
+		System.out.println("");
+		logger.info("=== PRUEBA "+(cont++)+" ======");
+	}
+
+	@AfterClass
+	public static void onceExecutedAfterAll() {
+		System.out.println("");
+		logger.info(">>> Terminado las pruebas");
+	}
+
+
+	/**
+	 * Parte testing
+	 * 
+	 */
+
+	@Test
+	public void testAltaJuegoVacio() {
+		logger.warn("Test::testAltaJuegoVacio(): []");
+	}
+	
+	@Test
+	public void testImprimirListaPorCategoriaVacio() {
+		logger.warn("Test::testImprimirListaPorCategoriaVacio(): []");
+	}	
+	
+}

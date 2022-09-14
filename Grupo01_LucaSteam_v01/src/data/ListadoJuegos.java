4aab174eb93fe824dedb91b7b84764d594076d3d
Ailed Nunez Ramos <nunezailed3@gmail.com>
2022-09-14 12:47:20 +0200

    alta juego
    lista juego opcion 2

--- Grupo01_LucaSteam_v01/src/data/ListadoJuegos.java
+++ Grupo01_LucaSteam_v01/src/data/ListadoJuegos.java
@@ -5,19 +5,22 @@ import java.util.ArrayList;
 import model.Juegos;
 import util.SacoFichero;
 
-public class ListadoJuegos implements InterfListadoJuegos{
-	
+public class ListadoJuegos implements InterfListadoJuegos {
+
 	ArrayList<Juegos> listaJuegos = new ArrayList<Juegos>();
-	
+
 	public void listaJuegosInicial() {
 		listaJuegos = SacoFichero.leeFichero("vgsales.csv");
 	}
-	
+
 	public void imprimirListaJuegos() {
-		for(Juegos j: listaJuegos) {
+		for (Juegos j : listaJuegos) {
 			System.out.println(j.toString());
 		}
 	}
-		
+
+	public void altaJuego(Juegos j) {
+		listaJuegos.add(j);
+	}
 
 }

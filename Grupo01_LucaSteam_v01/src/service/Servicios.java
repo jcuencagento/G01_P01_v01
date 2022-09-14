08943050829bf0925d1c23a2ab5268481028fa5e
Ailed Nunez Ramos <nunezailed3@gmail.com>
2022-09-14 12:48:12 +0200

    alta juego2
    FALTABA SERVICIOS

--- Grupo01_LucaSteam_v01/src/service/Servicios.java
+++ Grupo01_LucaSteam_v01/src/service/Servicios.java
@@ -1,23 +1,35 @@
 package service;
 
 import data.ListadoJuegos;
+import model.Juegos;
+import util.LecturaDatos;
 
-/**Clase de tipo Servicios que conecta el Control y los Datos...
+/**
+ * Clase de tipo Servicios que conecta el Control y los Datos...
  * 
  * @author Grupo 01
  *
  */
 
-public class Servicios implements InterfServicios{
-	
+public class Servicios implements InterfServicios {
+
 	ListadoJuegos lj = new ListadoJuegos();
-	
+
 	public void listaJuegosInicial() {
 		lj.listaJuegosInicial();
 	}
-	
+
 	public void imprimirListaJuegos() {
 		lj.imprimirListaJuegos();
 	}
-	
+
+	public void altaJuego() {
+		Juegos j = Juegos.creadorJuegos(LecturaDatos.leerString("Introduce el nombre "),
+				LecturaDatos.leerString("Introduce la platafoma "),
+				LecturaDatos.leerInt("Introduce el año "),
+				LecturaDatos.leerString("Introduce genero "),
+				LecturaDatos.leerString("Introduce publisher "));
+		lj.altaJuego(j);
+	}
+
 }

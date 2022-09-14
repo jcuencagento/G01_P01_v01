c02d6a9ba225d98e585c85fa2b0ae7adf4d1b4ca
Ailed Nunez Ramos <nunezailed3@gmail.com>
2022-09-14 10:01:03 +0200

    Update ailed
    agregue hola mundo desde mi rama

--- Grupo01_LucaSteam_v01/src/control/Main.java
+++ Grupo01_LucaSteam_v01/src/control/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		LucaSteam ls = new LucaSteam();
-		
+		System.out.println("Hola soy ailed desde mi rama");
 		
 		System.out.println("Bienvenido al administrador de LucaSteam!");
 		ls.entro();

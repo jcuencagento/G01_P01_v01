8207aa9378e42f421939c76f48123170c1afdddc
elinag9596 <113433249+elinag9596@users.noreply.github.com>
2022-09-14 10:10:22 +0200

    Nuevo print

--- Grupo01_LucaSteam_v01/src/control/Main.java
+++ Grupo01_LucaSteam_v01/src/control/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		LucaSteam ls = new LucaSteam();
 
 		System.out.println("Hola soy ailed desde mi rama");
-		
+		System.out.println("bfjsfs");
 		System.out.println("Bienvenido al administrador de LucaSteam!");
 		ls.entro();
 		System.out.println("Te despedimos del administrador de LucaSteam!");

418147e4b334c6084c022d2fb01ab0b2faa47014
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-09-14 16:26:03 +0200

    Commit TestSacoFichero
    Creo carpeta, package y Test + logger

---
+++ Grupo01_LucaSteam_v01/test/util/TestSacoFichero.java
@@ -0,0 +1,86 @@
+package util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.Juegos;
+
+import java.util.ArrayList;
+
+
+public class TestSacoFichero {
+	
+	/**
+	 * Parte logger
+	 * 
+	 */
+
+	private static byte cont = 1;
+	private static Logger logger;
+
+	// Inicializo
+	static {
+		try {
+			logger = LogManager.getLogger(TestSacoFichero.class);
+		} catch (Throwable e) {
+			System.out.println("Don't work");
+		}
+	}
+
+	@BeforeClass
+	public static void onceExecutedBeforeAll() {
+		logger.info(">>> Iniciando las pruebas");
+	}
+
+	@Before
+	public void executedBeforeEach() {
+		System.out.println("");
+		logger.info("=== PRUEBA "+(cont++)+" ======");
+	}
+
+	@AfterClass
+	public static void onceExecutedAfterAll() {
+		System.out.println("");
+		logger.info(">>> Terminado las pruebas");
+	}
+
+
+	/**
+	 * Parte testing
+	 * 
+	 */
+
+	@Test
+	public void testArchivoNulo() {
+		logger.warn("Test::testArchivoNulo(): []");
+		ArrayList<Juegos> listaExpected = new ArrayList<Juegos>();
+		ArrayList<Juegos> listaJuegos = SacoFichero.leeFichero(null);
+		Assert.assertEquals(listaExpected, listaJuegos);
+	}
+	
+	@Test
+	public void testArchivoInexistente() {
+		logger.warn("Test::testArchivoInexistente(): []");
+		ArrayList<Juegos> listaExpected = new ArrayList<Juegos>();
+		ArrayList<Juegos> listaJuegos = SacoFichero.leeFichero("holahola.txt");
+		Assert.assertEquals(listaExpected, listaJuegos);
+	}	
+	
+	@Test
+	public void testArchivoCorrecto() {
+		logger.info("Test::testArchivoCorrecto(): ArrayList<Juegos>");
+		
+		ArrayList<Juegos> listaExpected = new ArrayList<Juegos>();
+		ArrayList<Juegos> listaJuegos = SacoFichero.leeFichero("vgsales.csv");
+		Assert.assertNotEquals(listaExpected, listaJuegos);
+	}
+
+}
+
+
